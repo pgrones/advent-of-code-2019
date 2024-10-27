4e03b3237e1283f66efc359892c2a26ad6c98354
Leah Grones <pascal.grones@gmx.at>
2024-10-27 10:15:33 +0100

    cleanup computer

--- common.js
+++ common.js
@@ -1,49 +1,56 @@
 export class IntCodeComputer {
-  memory = [];
-  pointer = 0;
-  opCodes = [1, 2, 99];
+  #memory = [];
+  #pointer = 0;
 
-  constructor(program) {
-    this.memory = program;
+  get pointer() {
+    return this.#pointer;
   }
 
-  execute(opcode) {
+  get memory() {
+    return this.#memory;
+  }
+
+  constructor(program) {
+    this.#memory = program;
+  }
+
+  run() {
+    let opcode;
+    while ((opcode = this.#memory[this.#pointer]) !== 99) {
+      this.#execute(opcode);
+    }
+  }
+
+  #execute(opcode) {
     let params = [];
 
     switch (opcode) {
       case 1:
-        params = this.memory.slice(this.pointer + 1, this.pointer + 4);
-        this.add(...params);
+        params = this.#memory.slice(this.#pointer + 1, this.#pointer + 4);
+        this.#add(...params);
         break;
       case 2:
-        params = this.memory.slice(this.pointer + 1, this.pointer + 4);
-        this.multiply(...params);
+        params = this.#memory.slice(this.#pointer + 1, this.#pointer + 4);
+        this.#multiply(...params);
         break;
       default:
         throw new Error("Unknown opcode: " + opcode);
     }
 
-    this.pointer += params.length + 1;
+    this.#pointer += params.length + 1;
   }
 
-  add(posA, posB, posOut) {
-    const a = this.memory[posA];
-    const b = this.memory[posB];
+  #add(posA, posB, posOut) {
+    const a = this.#memory[posA];
+    const b = this.#memory[posB];
 
-    this.memory[posOut] = a + b;
+    this.#memory[posOut] = a + b;
   }
 
-  multiply(posA, posB, posOut) {
-    const a = this.memory[posA];
-    const b = this.memory[posB];
+  #multiply(posA, posB, posOut) {
+    const a = this.#memory[posA];
+    const b = this.#memory[posB];
 
-    this.memory[posOut] = a * b;
-  }
-
-  run() {
-    let opcode;
-    while ((opcode = this.memory[this.pointer]) !== 99) {
-      this.execute(opcode);
-    }
+    this.#memory[posOut] = a * b;
   }
 }
